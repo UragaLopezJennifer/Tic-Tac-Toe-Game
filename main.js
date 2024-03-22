66592e93a06ba06d8ffff5585aec6c3158a9e9c8
Jennifer Uraga Lopez <jenny@jennifers-mbp.mynetworksettings.com>
2024-03-22 13:09:56 -0400

    Taking turn function

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    // new code below
+    board[idx] = turn;
+    // check your console logs to make sure it's working!
+    console.log(board);
+ };
 function init() {
     board = [
     '', '', '',
@@ -16,9 +28,11 @@ function init() {
 };
 
 function render() {
-    board.forEach(function(mark, index){
-    console.log(mark, index);
+    board.forEach(function(mark, index) {
+    //this sets the text content of the square of the same position to the mark on the board. 
+    squares[index].textContent = mark;
     });
+        
 };
 
  //be sure to call the init function!
